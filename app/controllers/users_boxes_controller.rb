413f4659699daa295962fab5e3de3a333d0e93c1
ismurray <ians.murray@yahoo.com>
2018-04-16 16:26:07 -0400

    Verify write-access in UserBoxesController
    Add method to UserBoxesController to check whether the current user has
    write access for the Box they're trying to change permissions for.

--- app/controllers/users_boxes_controller.rb
+++ app/controllers/users_boxes_controller.rb
@@ -20,8 +20,8 @@ class UsersBoxesController < ProtectedController
 
   # POST /users_boxes
   def create
-    # first, build the UsersBox enter normally to ensure that the current user
-    # has write entry for the given Box, then change the user_id of @users_box
+    # first, build the UsersBox entry normally to ensure that the current user
+    # has write access for the given Box, then change the user_id of @users_box
     # to that of the user passed in the request
     @users_box = current_user.users_boxes.build(users_box_params)
     @users_box[:user_id] = users_box_params[:user_id]
@@ -48,13 +48,25 @@ class UsersBoxesController < ProtectedController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_users_box
-      @users_box = current_user.users_boxes.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def users_box_params
-      params.require(:users_box).permit(:user_id, :box_id, :write_access)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_users_box
+    return false unless current_user_has_write_access?
+    @users_box = UsersBox.find(params[:id])
+  end
+
+  # Returns true if the current user has write access in the given Box
+  def current_user_has_write_access?
+    @current_box = UsersBox.find(params[:id]).box
+    @current_user_permission = @current_box.users_boxes
+                                           .where(user_id: current_user[:id])
+                                           .first
+    @current_user_permission[:write_access]
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def users_box_params
+    p 'original params are ', params
+    params.require(:users_box).permit(:user_id, :box_id, :write_access)
+  end
 end
