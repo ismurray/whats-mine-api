fd755f966c96eb144925cd7bd5462794712eb208
ismurray <ians.murray@yahoo.com>
2018-04-16 21:14:40 -0400

    Add computed property `editable` to Box serializer
    Shows current user's write access for the current Box.

--- app/serializers/box_serializer.rb
+++ app/serializers/box_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class BoxSerializer < ActiveModel::Serializer
-  attributes :id, :name, :items, :users, :permissions
+  attributes :id, :name, :items, :users, :permissions, :editable
 
   def users
     object.users.pluck(:id)
@@ -13,5 +13,13 @@ class BoxSerializer < ActiveModel::Serializer
 
   def permissions
     object.users_boxes.pluck(:id)
+  end
+
+  # Returns (T/F) based on the user's write_access for the current Box
+  def editable
+    # find the users_box entry for the current Box and the current user
+    @users_box = object.users_boxes.where(user_id: scope.id).first
+    # return the write_access
+    @users_box[:write_access]
   end
 end
